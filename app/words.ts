ee064107d2ed372ec6b3ff8d37205c9e8a31e690
utk2602 <166751207+utk2602@users.noreply.github.com>
2024-12-23 08:18:06 +0530

    Your commit message

---
+++ app/words.ts
@@ -0,0 +1,19 @@
+const wordss =[
+    'the', 'be', 'to', 'of', 'and', 'a', 'in', 'that', 'have', 'I',
+    'it', 'for', 'not', 'on', 'with', 'he', 'as', 'you', 'do', 'at',
+    'this', 'but', 'his', 'by', 'from', 'they', 'we', 'say', 'her', 'she',
+    'or', 'an', 'will', 'my', 'one', 'all', 'would', 'there', 'their', 'what',
+    'so', 'up', 'out', 'if', 'about', 'who', 'get', 'which', 'go', 'me',
+    'when', 'make', 'can', 'like', 'time', 'no', 'just', 'him', 'know', 'take',
+    'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other',
+    'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also'
+]
+
+export function generateWords(count:number):string[]{
+    const word:string[]=[]
+    for(let i =0 ; i <count;i++){
+        const randomind=Math.floor(Math.random()*wordss.length)
+        word.push(wordss[randomind])
+    }
+    return word
+}
